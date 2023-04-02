1d242f53875dadb7ac901920bbff703bbda4e6b7
Nikita Struchkov <struchk0vnikita@yandex.ru>
2023-04-02 13:25:50 +0300

    Заполнение карточек из массива

--- script/cards.js
+++ script/cards.js
@@ -1,4 +1,4 @@
-const initialCards = [
+const initialCards = [  // начальный массив карточек
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -27,16 +27,29 @@ const initialCards = [
 
 
 const cardTemplate = document.getElementById('card-template');
+const cardSection = document.querySelector('.elements');  // сюда функция renderCardElement будет вставлять карточку
 console.log(cardTemplate);
+console.log(cardSection);
 
 // создаем функцию котор на осн данных создает элемент
 const createCardElement = (cardData) => {      // Создать элемент карточки
   // cardData это объект с данными о карточке
+const cardElement = cardTemplate.content.querySelector('.element').cloneNode(true);
+console.log(cardElement);
+const cardName = cardElement.querySelector('.element__name');
+const cardPhoto = cardElement.querySelector('.element__photo');
+
+cardName.textContent = cardData.name;
+cardPhoto.src = cardData.link;
+cardPhoto.alt = cardData.name;
+
+return cardElement;
 }
 
+const renderCardElement = (cardElement) => {  // добавление карточки в дерево
+cardSection.prepend(cardElement);
+};
 
-
-initialCards.forEach((card) => {
-  createCardElement(card);
+initialCards.forEach((card) => {      // итерация по массиву карточек
+  renderCardElement(createCardElement(card));
 });
-
