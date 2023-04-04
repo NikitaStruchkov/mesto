e4fa38eea12a1fea0624e5c95120bb64110ec269
Nikita Struchkov <struchk0vnikita@yandex.ru>
2023-04-04 23:01:35 +0300

    Добавление карточки

--- script/index.js
+++ script/index.js
@@ -1,25 +1,31 @@
-const editButton = document.querySelector('.profile__edit-button');
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
-const editPopup = document.querySelector('.popup');
-const editPopupCloseButton = editPopup.querySelector('.popup__close');
-const editPopupTitle = editPopup.querySelector('.popup__text-area_input_name');
-const editPopupSubtitle = editPopup.querySelector('.popup__text-area_input_job');
-const editPopupForm = editPopup.querySelector('.popup__content');
+const editButton = document.querySelector(".profile__edit-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const editPopup = document.getElementById("edit-popup");
+const editPopupCloseButton = document.getElementById("edit-btn-close");
+const editPopupTitle = editPopup.querySelector(".popup__text-area_input_name");
+const editPopupSubtitle = editPopup.querySelector(".popup__text-area_input_job");
+const editPopupForm = editPopup.querySelector(".popup__content_type_edit");
 
-function popupOpened() {
+const openPopup = (popup) => {
+  // функция, которая открывает popup-ы
+  popup.classList.add("popup_opened");
+};
+
+const closePopup = (popup) => {
+  // функция, которая закрывает popup-ы
+  popup.classList.remove("popup_opened");
+};
+
+editButton.addEventListener("click", () => {
+  openPopup(editPopup);
   editPopupTitle.value = profileTitle.textContent; // значение поля по умолчанию
   editPopupSubtitle.value = profileSubtitle.textContent; //значение поля по умолчанию
-  editPopup.classList.add('popup_opened');
-}
+});
 
-editButton.addEventListener('click', popupOpened); //вызываем ранее объявленую функцию
-
-function popupClosed() {
-  editPopup.classList.remove('popup_opened');
-}
-
-editPopupCloseButton.addEventListener('click', popupClosed); //вызываем ранее объявленую функцию
+editPopupCloseButton.addEventListener("click", () => {
+  closePopup(editPopup);
+});
 
 function handleFormSubmit(event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -28,27 +34,45 @@ function handleFormSubmit(event) {
 
   profileTitle.textContent = name; // меняем имя в профиле
   profileSubtitle.textContent = job;
-  editPopup.classList.remove('popup_opened'); // закрываем попап сразу после submit-а
+  editPopup.classList.remove("popup_opened"); // закрываем попап сразу после submit-а
 }
 
 // добавим обработчик submit-а
-editPopupForm.addEventListener('submit', handleFormSubmit); //вызываем ранее объявленую функцию
-
+editPopupForm.addEventListener("submit", handleFormSubmit); //вызываем ранее объявленую функцию
 
 // --------------------------- Форма добавления карточки
 
-const addPopup = document.querySelector('.add-popup');
-const addButton = document.querySelector('.profile__add-button');
-const addPopupCloseButton = addPopup.querySelector('.add-closed');
+const addButton = document.querySelector(".profile__add-button");
+const addPopup = document.getElementById("add-popup");
+const addPopupCloseButton = document.getElementById("add-btn-close");
 
-function addCardOpened() {    // функция, которая открывает попап
-addPopup.classList.add('popup_opened');
+addButton.addEventListener("click", () => {
+  openPopup(addPopup);
+});
+
+addPopupCloseButton.addEventListener("click", () => {
+  closePopup(addPopup);
+});
+
+
+// --------------------------- Добавление карточки
+const addPopupForm = addPopup.querySelector(".popup__content_type_add");
+
+const handleCardSubmit = (event) => {
+  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  const nameInput = addPopupForm.querySelector('.popup__text-area_card_name');
+  const urlInput = addPopupForm.querySelector('.popup__text-area_card_url');
+
+  const name = nameInput.value;
+  const link = urlInput.value;
+
+  const cardData = {
+    name,
+    link
+  };
+  renderCardElement(createCardElement(cardData));    // еще раз повторить последовательность от и до
+  closePopup(addPopup);
 }
 
-addButton.addEventListener('click', addCardOpened);
-
-function addCardClosed() {    // функция, которая закрывает попап
-  addPopup.classList.remove('popup_opened');
-}
-
-addPopupCloseButton.addEventListener('click', addCardClosed);
+addPopupForm.addEventListener('submit', handleCardSubmit);
